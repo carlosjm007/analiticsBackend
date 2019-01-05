4f12b5844bff04b6e845186a0e609d917ccb1492
carlosj.manosalvar@gmail.com <renault4>
2019-01-04 23:01:31 -0500

    task parametric model were added

--- analitics/load_parametric.sh
+++ analitics/load_parametric.sh
@@ -1,6 +1,7 @@
+sudo python3 manage.py loaddata parametric/Pais.json
+sudo python3 manage.py loaddata parametric/Ciudad.json
 sudo python3 manage.py loaddata parametric/HourDay.json
 sudo python3 manage.py loaddata parametric/DayWeek.json
 sudo python3 manage.py loaddata parametric/PaginaCiudad.json
 sudo python3 manage.py loaddata parametric/Urls.json
-sudo python3 manage.py loaddata parametric/Pais.json
-sudo python3 manage.py loaddata parametric/Ciudad.json
+sudo python3 manage.py loaddata parametric/task.json
